8563bc7e4b60ac173e24f399ee77d23a135cfbcc
Ed Singleton <singletoned@gmail.com>
2022-07-13 16:39:43 +0100

    Restrict http methods

--- cei/entity/views.py
+++ cei/entity/views.py
@@ -4,5 +4,6 @@ from . import models, serializers
 
 
 class EntityViewSet(viewsets.ModelViewSet):
+    http_method_names = ("get", "post")
     queryset = models.Entity.objects.all().order_by("-created_at")
     serializer_class = serializers.EntitySerializer
