c605df930e3fc358374cfd51562c3c0cd576c87e
Ed Singleton <singletoned@gmail.com>
2022-07-13 15:27:41 +0100

    Rename app to entity

--- cei/entity/apps.py
+++ cei/entity/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class IdentifierConfig(AppConfig):
+class EntityConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'identifier'
+    name = 'entity'
