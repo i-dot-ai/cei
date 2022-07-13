4dda89b2bcdacbf5e7114c417c8021238c52c046
Ed Singleton <singletoned@gmail.com>
2022-07-13 15:29:16 +0100

    Add Entity model

--- cei/entity/models.py
+++ cei/entity/models.py
@@ -1,3 +1,14 @@
+import datetime
+import uuid
+
+import pytz
 from django.db import models
 
-# Create your models here.
+
+def now():
+    return datetime.datetime.now(tz=pytz.UTC)
+
+
+class Entity(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(editable=False, default=now)
